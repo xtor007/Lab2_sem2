d2388d68beff923bc2f7b611d6731f87b39ab461
berlinsky <berliniumyar@gmail.com>
2021-02-22 20:40:39 +0200

    a problem with importing prototypes was solved

--- Lab2/Lab2/counting.hpp
+++ Lab2/Lab2/counting.hpp
@@ -5,10 +5,13 @@
 //  Created by Anatoliy Khramchenko on 2/22/21.
 //  Copyright © 2021 Anatoliy Khramchenko. All rights reserved.
 //
-
+#include <iostream>
 #ifndef counting_hpp
 #define counting_hpp
 
 #include <stdio.h>
 
 #endif /* counting_hpp */
+using namespace std;
+
+int stringLength(string);
