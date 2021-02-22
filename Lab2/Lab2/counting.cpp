7ae9af19fcd9393dbefb00df33ebf080754bf4ac
berlinsky <berliniumyar@gmail.com>
2021-02-22 20:37:53 +0200

    1st function in counting.cpp

--- Lab2/Lab2/counting.cpp
+++ Lab2/Lab2/counting.cpp
@@ -1,4 +1,11 @@
-//
+// "/Users/yaroslav/Desktop/Prepar/Lab2_sem2/files/" -> for argument "teka"
 //  counting.cpp
-
 #include "counting.hpp"
+
+int stringLength(string s){
+    int size = 0;
+    while (s[size]){
+        size++;
+    }
+    return size;
+}
