d1de595718c6e290e41e6fdc3a2f9c6cc4bb05ae
Anatoliy <tolxpams@gmail.com>
2021-02-22 17:40:05 +0200

    completed but did not test

--- Lab2/Lab2/reading.cpp
+++ Lab2/Lab2/reading.cpp
@@ -9,11 +9,25 @@
 #include "reading.hpp"
 
 vector<string> readFiles(int *kolTeams) {
+    kolTeams = 0;
     string teka;
     cin >> teka;
     teka += "premier_league";
     vector<string> resultRead;
     int countFiles = 1;
+    string nowFile = teka + to_string(countFiles) + ".csv";
+    string s;
+    int countTeamInFile;
+    ifstream inFile;
+    inFile.open(nowFile);
+    while (inFile.is_open()) {
+        getline(inFile,s,'\0');
+        countTeamInFile = stoi(s);
+        for (kolTeams; kolTeams<kolTeams+countTeamInFile; kolTeams++) {
+            getline(inFile,s,'\0');
+            resultRead.push_back(s);
+        }
+    }
 //    /Users/khramchenko/Documents/Lab2_sem2/files/premier_league2.csv
     return resultRead;
 }
