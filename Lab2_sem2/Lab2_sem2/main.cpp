bd65015fd32b7d614eb0025ba08e7263ff9ba824
berlinsky <berliniumyar@gmail.com>
2021-02-23 21:39:02 +0200

    Fixing arguments in output

--- Lab2_sem2/Lab2_sem2/main.cpp
+++ Lab2_sem2/Lab2_sem2/main.cpp
@@ -25,10 +25,10 @@ int main(int argc, const char * argv[]) {
     int cancels[n];          //numbers of canceled balls for each team
     int difference[n];       //numbers of difference between canceled balls and goals(for case when num of                          points from one team is equal to num of points from another team
     int goals[n];            //numbers of goals for each team
-    int points[n];           //points for each team
+    int array[n];           //points for each team
     
-    getScore(mtx, n, teams, goals, difference, cancels, points);    // a func for getting array of points
-    sort(points, difference, goals, teams, n);                      // a func for sorting all the arrays
-    outputInFile(path, teams, goals, difference, points, n);        // outputing sorted arrays in file
+    getScore(mtx, n, teams, goals, difference, cancels, array);    // a func for getting array of points
+    sort(array, difference, goals, teams, n);                      // a func for sorting all the arrays
+    outputInFile(path, teams, goals, cancels, array, n);        // outputing sorted arrays in file
     return 0;
 }
