24f6c381f452e97454e898e7cec90f1dcf8646be
berlinsky <berliniumyar@gmail.com>
2021-02-23 18:31:33 +0200

    Function of output added(functional)

--- Lab2_sem2/Lab2_sem2/output.cpp
+++ Lab2_sem2/Lab2_sem2/output.cpp
@@ -3,11 +3,13 @@
 
 #include "output.hpp"
 
-void outputInFile(string *teams, int *GoalsArray, int *CancelsArray, int *PointsArray){
+void outputInFile(string *teams, int *GoalsArray, int *CancelsArray, int *PointsArray, int size){
     ofstream fileOut;
     fileOut.open("/Users/yaroslav/Desktop/Prepar/Lab2_sem2/files/result.csv");
     if (fileOut.is_open()){
-        
+        for(int i = 0; i < size; i++){
+            fileOut<<i<<". "<<teams[i]<<" - "<<GoalsArray[i]<<":"<<CancelsArray[i]<<"     "<<PointsArray[i]<<endl;
+        }
     }
     else cout<<"Cannot open the file or it couldn't be created. Try to do it again!\n";
 
